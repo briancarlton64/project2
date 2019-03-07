d17e216d8ff57ad2deea00edb92bd74fb7a3d01b
Brian Carlton <brian.carlton@ou.edu>
2019-03-07 12:31:08 -0600

    MesoAbstract doc 2019-03-07 1231p

--- src/MesoAbstract.java
+++ src/MesoAbstract.java
@@ -1,8 +1,32 @@
+/**
+ * An abstract class with two abstract methods. 
+ * Sub classes will implement the methods as needed.
+ * MesoInherit is a subclass of MesoAbstract.
+ * 
+ * @author Brian Carlton
+ * @version 2019-03-07
+ */
 public abstract class MesoAbstract
-{
-    MesoStation currentStation;
-    
-    public abstract int[] calAverage();
+{    
+    /**
+     * A char array used in the following methods.
+     */
+	char[] st;
+	/**
+     * A method to calculate the average value of an array.
+     * 
+     * @return a int array formatted as follows:
+     * 				int[0] The average (rounded) value.
+     * 				int[1] The ceiling of the average.
+     * 				int[2] The floor of the average.
+     */
+	public abstract int[] calAverage();
+	
+	/**
+	 * 	A method to return a char representation of
+	 * 	calAverage()[0] (i.e. the average char of the array.)
+	 * 
+	 * @return the average char of the array.
+	 */
     public abstract char letterAverage();
-
 }
